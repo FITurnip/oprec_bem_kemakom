aa9b8d9964ecb4f87d25800edd7846dd275515f2
FITurnip <franklinirtr@gmail.com>
2024-02-20 08:09:17 +0700

    preventif sql injection

--- database/database.php
+++ database/database.php
@@ -14,42 +14,44 @@ function connectDB() {
 
 function getCapengurus($nim) {
     $conn = connectDB();
-    $query = "SELECT * FROM Capengurus WHERE nim = $nim limit 1;";
-    $queryResult = $conn->query($query);
-    $result = null;
 
-    if ($queryResult->num_rows > 0) {
-        while($row = $queryResult->fetch_assoc()) {
-            $result = $row;
-        }
-    } else {
-        echo "0 queryResults";
-    }
-    $conn->close();
+    if($conn->connect_error) die("connection is failed");
+
+    $stmt = $conn->prepare('SELECT * FROM Capengurus WHERE nim = ? limit 1');
+    $stmt->bind_param('s', $nim);
+    $stmt->execute();
+    $queryResult = $stmt->get_result();
+
+    $result = $queryResult->fetch_assoc();
     
+    $conn->close();
     return $result;
 }
 
 
 function getUcapanMotivasi($kelolosan) {
     $conn = connectDB();
-    if($conn->connect_error) {
-        die("connection is failed");
-    }
+
+    if($conn->connect_error) die("connection is failed");
+
     $time = time();
-    $query = "SELECT * FROM UcapanMotivasi WHERE kelolosan = $kelolosan ORDER BY RAND($time) LIMIT 1;";
-    $queryResult = $conn->query($query);
-    $result = null;
+    $stmt = $conn->prepare('SELECT * FROM UcapanMotivasi WHERE kelolosan = ? ORDER BY RAND(?) LIMIT 1');
+    $stmt->bind_param('ss', $kelolosan, $time);
+    $stmt->execute();
+    $queryResult = $stmt->get_result();
 
-    if ($queryResult->num_rows > 0) {
-        while($row = $queryResult->fetch_assoc()) {
-            $result = $row;
-        }
-    } else {
-        echo "0 queryResults";
-    }
+    $result = $queryResult->fetch_assoc();
+    
     $conn->close();
-
     return $result;
 }
+
+// hastag sinariaksi salah
+// tombol close reload terus
+// javascriptnya belum ngehandle kalau datanya g ada
+// jangan pas klik close malah reload bang
+// ini urang ubah phpnya buat ngehindari sql injection. takutnya ada yang ngebobol database kemakom
+// background belum responsive sesuai kesepakatan awal
+// prevent enter when input nim
+// gambar di tengah modal, ganti
 ?>
